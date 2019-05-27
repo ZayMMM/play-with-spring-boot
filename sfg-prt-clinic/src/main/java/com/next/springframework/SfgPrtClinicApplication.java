cc2ea42eed3bb57669e6e871b439dac815591d75
ZayMaungMaungMyint <ZayMaungMaungMyint@192.168.0.106>
2019-05-27 21:02:45 +0630

    Initial Project Add

---
+++ sfg-prt-clinic/src/main/java/com/next/springframework/SfgPrtClinicApplication.java
@@ -0,0 +1,13 @@
+package com.next.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPrtClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPrtClinicApplication.class, args);
+	}
+
+}
